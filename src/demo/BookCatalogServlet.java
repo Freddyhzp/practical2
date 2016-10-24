231f316cd703b9691c03be3d4ff1fc34ff841e36
143808a <iamawesome2201@gmail.com>
2016-10-24 16:58:41 +0800

    Initial commit

---
+++ src/demo/BookCatalogServlet.java
@@ -0,0 +1,35 @@
+package demo;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+/**
+ * Created by 143808a on 10/24/2016.
+ */
+@WebServlet(name = "BookCatalogServlet", urlPatterns = "/bookcatalog")
+public class BookCatalogServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        BookDBAO bda = new BookDBAO();
+
+        ArrayList<BookDetails> allbooks = bda.getAllBooks();
+
+
+        for(int p = 0; p > allbooks.size(); p++ ){
+            BookDetails bd = bda.getBookDetails(""+p+"");
+            allbooks.get(p);
+        }
+        PrintWriter out = response.getWriter();
+        out.println("");
+
+    }
+}
